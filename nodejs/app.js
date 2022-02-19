4ab75ba4fc739229e326e5a8b443656d26ec7783
Taichir0923 <54131730+Taichir0923@users.noreply.github.com>
2022-02-19 09:21:14 -0500

    update

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,35 +1,34 @@
-const http = require("http");
-const fileSystem = require('fs');
+const express = require('express');
+const app = express();
 
-const server = http.createServer(function(request , response){
-    if(request.url === '/'){
-        fileSystem.readFile('./test.txt' , 'utf-8' , (err , data) => {
-            // console.log(data)
-            response.end(data);
-        })
-    } else if (request.url === '/about'){
-        const userData = {
-            ner: "Narada",
-            email: "narada@mail.com",
-            utas: 99009900
-        }
-        fileSystem.writeFile('./user.json' , JSON.stringify(userData) , () => {
-            // console.log(data)
-            response.end('hereglegch uusgegdlee');
-        })
-        // fileSystem.writeFileSync('./test.txt' , "file update");
-    } else {
-        response.end('404 page not found')
-    }
+app.set('view engine' , 'ejs');
+app.set('views' , 'pages');
+
+app.get('/' , (req, res) => {
+    res.render('index' , {
+        data: ["Hi there" , "hello" , 123]
+    })
+});
+
+app.get('/home' , (req, res) => {
+    res.render('home' , {
+        title: 'homepage'
+    })
 })
 
-// writeFile - asynchronous
-// writeFileSync - synchronous
+app.get('/about' , (req, res) => {
+    res.end('about page')
+})
 
-server.listen(3000);
+app.listen(3000 , () => {
+    console.log('server started at port 3000')
+});
 
-// Гэрт express js судлах
+// server side template , pug template , clear template
 
-// npm - node package management
 
-// github.com/taichir0923/nodejs-weekend
+// <%= utga avna %>
+// <% js code %>
+// <%- html parse %>
+
+// post 
