1a5321d7c2d63d7db6c1aa789335b3f098688415
Taichir0923 <54131730+Taichir0923@users.noreply.github.com>
2022-02-27 15:18:03 +0800

    update

--- nodejs/app.js
+++ nodejs/app.js
@@ -11,7 +11,7 @@ app.set('views' , 'pages');
 app.use(bp.urlencoded({extended: false}));
 
 app.get('/' , (req, res) => {
-    fs.readFile(path.join(__dirname , '/data' , '/users.json') , 'utf-8' , (err , data) => {
+    fs.readFile(path.join(__dirname , '/data' , '/download.json') , 'utf-8' , (err , data) => {
         if(!err){
             const users = JSON.parse(data);
             res.render('index' , {
